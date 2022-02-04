59dd29c51a165ae2a5eae037796f6801a1ccc157
Mateus Dall´Agnol <mateusdall12@gmail.com>
2022-02-04 00:38:33 -0300

    add
    s

--- lanchonete.py
+++ lanchonete.py
@@ -1,7 +1,8 @@
 ##original
 import sys
 
-print("Lanchonete")
+print(20*"-"+ "Lanchonete" + 20*"-")
+print("")
 
 cardapio1 = []
 lanches = {"pastel": 3.50, "coxinha": 4.50}
@@ -11,11 +12,8 @@ cardapio2 = []
 bebidas = {"coca lata": 3.50, "suco": 4.25}
 cardapio2.append(bebidas)
 
-quantidade = []
-carrinho = []
-total = []
+
 while True:
-    try:
         
 
         pedido = str(input("Qual o seu pedido?: Lanche ou Refri? ")) #Pedido
@@ -30,17 +28,27 @@ while True:
                     print(a + ": "+str(b))
                 print(16*"-")
 
+                lanchep = str(input("Qual lanche voce quer "))
+                if lanchep == "pastel":
+                    print("Deu 3.50")
+                if lanchep == "coxinha":
+                    print("Deu 4.50") 
+                    
+        if pedido == 'refri':
 
+            print("")
+            print("Cardapio de lanches")
+            print(16*"-")
+            
+            for bebida in cardapio2:
+                for a, b in bebida.items():
+                    print(a + ": "+str(b))
+                print(16*"-")
 
-                lan = str(input("Escolha um lanche: "))
-                carrinho.append(lan)
-                if lan == ("pastel"):
-                        quanti=int(input("Quantos pasteis são? "))
-                        quantidade.append(quanti)
-                
-                pagar = quanti * 3.50
-                ##print(pagar)
-                total.append(pagar)
-                pagar_total=sum(total)
-    except:
-        print('error')
+                bebidap = str(input("Qual bebida voce quer "))
+                if bebidap == "suco":
+                    print("Deu 4.25")
+                if bebidap == "coca lata":
+                    print("Deu 3.50")
+        break
+
